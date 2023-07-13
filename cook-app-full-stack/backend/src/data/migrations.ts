a8a4d4f227dd747c76b15b3d83d348cf0feec9ab
aversii <lucasaversi@gmail.com>
2023-07-13 01:12:26 -0300

    SQL tables recipes, follows, likes create and populate by running npm run migrations

--- cook-app-full-stack/backend/src/data/migrations.ts
+++ cook-app-full-stack/backend/src/data/migrations.ts
@@ -1,5 +1,9 @@
 import connection from "./connection";
 import users from "./tables/users.json"
+import recipes from "./tables/recipes.json"
+import likes from "./tables/likes.json"
+import follows from "./tables/follows.json"
+
 
 const printError = (error: any) => { console.log(error.sqlMessage || error.message) };
 
@@ -12,17 +16,62 @@ const createTables = () => connection
       password VARCHAR(255) NOT NULL,
       role ENUM ("NORMAL","ADMIN") NOT NULL DEFAULT ("NORMAL")
    );
-   
+
+   CREATE TABLE IF NOT EXISTS cookApp_recipes(
+      id VARCHAR(255) PRIMARY KEY,
+      title VARCHAR(255) NOT NULL,
+      photo VARCHAR(255),
+      description VARCHAR(255) NOT NULL,
+      cook_time VARCHAR(63),
+      servings VARCHAR(63),
+      ingredients VARCHAR(255) NOT NULL,
+      instructions VARCHAR(255) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      author_id VARCHAR(255) NOT NULL,
+      FOREIGN KEY (author_id) REFERENCES cookApp_users (id)
+   );
+
+   CREATE TABLE IF NOT EXISTS cookApp_follows(
+      user_id VARCHAR(255) NOT NULL,
+      following_id VARCHAR(255) NOT NULL,
+      FOREIGN KEY (user_id) REFERENCES cookApp_users (id),
+      FOREIGN KEY (following_id) REFERENCES cookApp_users (id),
+      PRIMARY KEY (user_id, following_id)
+   );
+
+   CREATE TABLE IF NOT EXISTS cookApp_likes(
+      user_id VARCHAR(255) NOT NULL,
+      recipe_id VARCHAR(255) NOT NULL,
+      FOREIGN KEY (user_id) REFERENCES cookApp_users (id),
+      FOREIGN KEY (recipe_id) REFERENCES cookApp_recipes (id),
+      PRIMARY KEY (user_id, recipe_id)
+   );  
    
    `)
-   .then(() => { console.log("A tabela foi criada!") })
+
+
+   .then(() => { console.log("As tabelas foram criadas!") })
    .catch(printError);
 
 const insertUsers = () => connection("cookApp_users")
    .insert(users)
-   .then(() => { console.log("Tabela users foi populada com sucesso!!") })
+   .then(() => { console.log("Tabela users foi criada e populada com sucesso!!") })
    .catch(printError);
 
+const insertRecipes = () => connection("cookApp_recipes")  
+   .insert(recipes)
+   .then(() => { console.log("Tabela recipes foi criada e populada com sucesso!!") })
+   .catch(printError);
+
+   const insertLikes = () => connection("cookApp_likes")  
+   .insert(likes)
+   .then(() => { console.log("Tabela likes foi criada e populada com sucesso!!") })
+   .catch(printError);
+
+   const insertFollows = () => connection("cookApp_follows")  
+   .insert(follows)
+   .then(() => { console.log("Tabela follows foi criada e populada com sucesso!!") })
+   .catch(printError);
 
 
 
@@ -30,4 +79,7 @@ const closeConnection = () => { connection.destroy() }
 
 createTables()
    .then(insertUsers) 
+   .then(insertRecipes)
+   .then(insertFollows)
+   .then(insertLikes)
    .finally(closeConnection);
