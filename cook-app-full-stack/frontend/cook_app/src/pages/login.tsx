cb8feac8af179e4b6aea1cfdfc970f6aa11f414d
aversii <lucasaversi@gmail.com>
2024-03-04 09:26:56 -0300

    feat: Login page created

---
+++ cook-app-full-stack/frontend/cook_app/src/pages/login.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    console.log("tentou logar")
+    //router.push('/');
+  };
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button onClick={handleSubmit}>Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
